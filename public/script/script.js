484ff6783523a71f53bcc83528e06e9b40c02897
Selene Munoz <selene_mgr@outlook.com>
2023-10-04 12:28:01 -0400

    Creation nav-bar-side and bannerpartial
    Creation of nav-bar-side and banner partials instead of each page. As well as script to change the nav-bar-side items depending on the page selected.

--- public/script/script.js
+++ public/script/script.js
@@ -18,6 +18,12 @@ document.addEventListener('DOMContentLoaded', function () {
             setActiveClass(href);
         });
     });
+    
+    // Change banner message
+    changeBannerMessage(path);
+
+    // Change items descriptions for selection on nav-bar-side
+    changeNavBarItems(path); 
 });
 
 function setActiveClass(path) {
@@ -28,7 +34,6 @@ function setActiveClass(path) {
     }
 }
 
-
 // Method to scroll and center the content of the nav-list-side
 function scrollToSection(sectionId) {
     const offset = -window.innerHeight / 5; // Adjust this value to control the offset
@@ -38,3 +43,49 @@ function scrollToSection(sectionId) {
       top: targetElement.offsetTop + offset,
     });
 }
+
+function changeBannerMessage(path){
+    const bannerMessage = document.getElementById('bannerMessage');
+
+    // Check current page to change the message displayed on banner
+    if (path === '/'){
+        bannerMessage.innerHTML = "<b> Welcome Visitor... </b><br/> My name is <b>[ Selene ]</b> and this is my <i>Life</i>, I'm going to show you who I am and what I have created so far.";
+    }
+    else if (path === '/about') {
+        bannerMessage.innerHTML = "About ";
+    } 
+    else if (path === '/projects'){
+        bannerMessage.innerHTML = "Projects";
+    } 
+    else if (path === '/services'){    
+        bannerMessage.innerHTML = "Services";
+    } 
+    else if (path === '/contact'){
+        bannerMessage.innerHTML = "Contact";
+    };
+};
+
+function changeNavBarItems(path){
+    const navBarItems = document.getElementById('navbar-side');
+    let items = navBarItems.querySelectorAll(".nav-link");
+    
+    // Check current page to change the message displayed on banner
+    if (path === '/'){
+        items[0].innerHTML = "About Me";
+        items[1].innerHTML = "Projects";
+        items[2].innerHTML = "Services";
+        items[3].innerHTML = "Contact Me";
+    }
+    else if (path === '/projects'){
+        items[0].innerHTML = "Project 1";
+        items[1].innerHTML = "Project 2";
+        items[2].innerHTML = "Project 3";
+        items[3].innerHTML = "Project 4";
+    } 
+    else if (path === '/services'){    
+        items[0].innerHTML = "Service 1";
+        items[1].innerHTML = "Service 2";
+        items[2].innerHTML = "Service 3";
+        items[3].innerHTML = "Service 4";
+    } 
+};
