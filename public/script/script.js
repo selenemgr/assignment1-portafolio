e21264ef3355b2f2cc1a7152ab45699ce54ebd49
Selene Munoz <selene_mgr@outlook.com>
2023-10-04 22:00:41 -0400

    Creation About Me Page

--- public/script/script.js
+++ public/script/script.js
@@ -1,4 +1,3 @@
-
 document.addEventListener('DOMContentLoaded', function () {
     // Get the current path from the URL
     const path = window.location.pathname;
@@ -24,6 +23,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Change items descriptions for selection on nav-bar-side
     changeNavBarItems(path); 
+      
 });
 
 function setActiveClass(path) {
@@ -36,7 +36,7 @@ function setActiveClass(path) {
 
 // Method to scroll and center the content of the nav-list-side
 function scrollToSection(sectionId) {
-    const offset = -window.innerHeight / 5; // Adjust this value to control the offset
+    const offset = -window.innerHeight / 5; 
     const targetElement = document.querySelector(sectionId);
 
     window.scrollTo({
@@ -49,19 +49,22 @@ function changeBannerMessage(path){
 
     // Check current page to change the message displayed on banner
     if (path === '/'){
-        bannerMessage.innerHTML = "<b> Welcome Visitor... </b><br/> My name is <b>[ Selene ]</b> and this is my <i>Life</i>, I'm going to show you who I am and what I have created so far.";
+        bannerMessage.innerHTML = '<b class="code-color"> Welcome Visitor... </b><br/> My name is <b class="code-color">[ Selene ]</b> and this is my <i>Life</i>, I am going to show you who I am and what I have created so far.';
     }
     else if (path === '/about') {
-        bannerMessage.innerHTML = "About ";
+        bannerMessage.innerHTML = '<figure><blockquote class="blockquote fs-2"><p><i class="fa fa-quote-left fs-big code-color me-2"></i> When you help someone, you help everyone. <i class="fa fa-quote-right fs-big code-color me-2"></i></p></blockquote><figcaption class="blockquote-footer fs-4">Aunt May, <cite title="Source Title">Marvel’s Spider-Man</cite></figcaption></figure>';
     } 
     else if (path === '/projects'){
         bannerMessage.innerHTML = "Projects";
+        bannerMessage.innerHTML = '<figure><blockquote class="blockquote fs-2"><p><i class="fa fa-quote-left fs-big code-color me-2"></i> What is a man but the sum of his memories? We are the stories we live, the tales we tell ourselves. <i class="fa fa-quote-right fs-big code-color me-2"></i></p></blockquote><figcaption class="blockquote-footer fs-4">Clay Kaczmarek, <cite title="Source Title">Assassin’s Creed: Revelations</cite></figcaption></figure>';
     } 
     else if (path === '/services'){    
         bannerMessage.innerHTML = "Services";
+        bannerMessage.innerHTML = '<figure><blockquote class="blockquote fs-2"><p><i class="fa fa-quote-left fs-big code-color me-2"></i> What is better – to be born good, or to overcome your evil nature through great effort? <i class="fa fa-quote-right fs-big code-color me-2"></i></p></blockquote><figcaption class="blockquote-footer fs-4">Dragon Paarthurnax, <cite title="Source Title">The Elder Scrolls V: Skyrim</cite></figcaption></figure>';
     } 
     else if (path === '/contact'){
         bannerMessage.innerHTML = "Contact";
+        bannerMessage.innerHTML = '<figure><blockquote class="blockquote fs-2"><p><i class="fa fa-quote-left fs-big code-color me-2"></i> Waka Waka Waka <i class="fa fa-quote-right fs-big code-color me-2"></i></p></blockquote><figcaption class="blockquote-footer fs-4">Pac-Man, <cite title="Source Title">Pac-Man</cite></figcaption></figure>';
     };
 };
 
@@ -69,12 +72,18 @@ function changeNavBarItems(path){
     const navBarItems = document.getElementById('navbar-side');
     let items = navBarItems.querySelectorAll(".nav-link");
     
-    // Check current page to change the message displayed on banner
+    // Check current page to change the value of the side navigation bar
     if (path === '/'){
         items[0].innerHTML = "About Me";
         items[1].innerHTML = "Projects";
         items[2].innerHTML = "Services";
-        items[3].innerHTML = "Contact Me";
+        items[3].innerHTML = "Contact";
+    }
+    else if (path === '/about'){
+        items[0].innerHTML = "Videogames";
+        items[1].innerHTML = "Fashion";
+        items[2].innerHTML = "Make Up";
+        items[3].innerHTML = "Animals";
     }
     else if (path === '/projects'){
         items[0].innerHTML = "Project 1";
